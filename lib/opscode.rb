911a7444e3eb66a0cbd45ccadb46604aa72bd7a1
Fletcher Nichol <fnichol@nichol.ca>
2014-04-01 09:06:56 -0600

    Initial implementation.

--- lib/opscode.rb
+++ lib/opscode.rb
@@ -1,5 +1,28 @@
 require "opscode/version"
 
 module Opscode
-  # Your code goes here...
+
+  class CLI
+
+    attr_reader :argv
+
+    def initialize(argv, opts = {})
+      @stdout_io = opts[:stdout_io] || $stdout
+      @scm = opts[:scm] || "git"
+      @argv = Array(argv)
+    end
+
+    def run
+      case argv.first
+      when "version", "--version", "-v", "-version"
+        stdout_io.puts "Opscode version #{Opscode::VERSION}"
+      else
+        exec(scm, *argv)
+      end
+    end
+
+    private
+
+    attr_reader :stdout_io, :scm
+  end
 end
--- spec/lib/opscode_spec.rb
+++ spec/lib/opscode_spec.rb
@@ -1,7 +1,37 @@
-require 'spec_helper'
+require "spec_helper"
+require "stringio"
 
 describe Opscode do
+
   it 'has a version number' do
     expect(Opscode::VERSION).not_to be nil
+  end
+end
+
+describe Opscode::CLI do
+
+  let(:io) { StringIO.new }
+
+  it "#argv returns arguments" do
+    expect(Opscode::CLI.new(%w{one two three}).argv).to eq(%w{one two three})
+  end
+
+  %w{version --version -v -version}.each do |arg|
+    it "with argv '#{arg}' returns a version string" do
+      Opscode::CLI.new([arg], stdout_io: io).run
+      expect(io.string).to eq("Opscode version #{Opscode::VERSION}\n")
+    end
+  end
+
+  it "with any other arguments delegate to the SCM tool" do
+    cli = Opscode::CLI.new(%w{checkout mainline}, stdout_io: io, scm: "cvs")
+    expect(cli).to receive(:exec).with("cvs", "checkout", "mainline")
+    cli.run
+  end
+
+  it "scm tool defaults to git" do
+    cli = Opscode::CLI.new(%w{log}, stdout_io: io)
+    expect(cli).to receive(:exec).with("git", "log")
+    cli.run
   end
 end
